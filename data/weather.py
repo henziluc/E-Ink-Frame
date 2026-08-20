a72526879a3a562bb4b63cb4ea9bc6301e9dfd00
lhenz <henzi.luca@gmail.com>
2026-08-20 21:17:41 +0200

    Set up project structure and added some dummy code

---
+++ data/weather.py
@@ -0,0 +1,12 @@
+def get_weather():
+
+    # API request here
+
+    return {
+        "temperature": 24,
+        "condition": "Partly cloudy",
+        "high": 27,
+        "low": 16,
+        "humidity": 52,
+        "uv": 5
+    }
---
+++ main.py
@@ -0,0 +1,13 @@
+from data.weather import get_weather
+import config
+
+
+def main():
+
+    weather = get_weather()
+    
+  
+
+
+if __name__ == "__main__":
+    main()
