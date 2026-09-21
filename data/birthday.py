7206c7c3e5ebae8928a5a23e24ac4eb4ab758638
lhenz <henzi.luca@gmail.com>
2026-09-21 20:32:46 +0200

    fixed path error of birthday file

--- data/birthday.py
+++ data/birthday.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def get_birthday_data():
 
-    file_path = Path(__file__).resolve().parent / 'assets' / 'birthdays' / "birthday_data.json"
+    file_path = Path(__file__).resolve().parent.parent / 'assets' / 'birthdays' / "birthday_data.json"
 
     with open(file_path, "r", encoding="utf-8") as f:
         data = json.load(f)
