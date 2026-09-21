dffe0b1e4d51257b10adfbca7cbd0a5bac4eb535
lhenz <henzi.luca@gmail.com>
2026-09-21 20:03:35 +0200

    removed diggit after comma from sleep minutes

--- layout/health_widget.py
+++ layout/health_widget.py
@@ -103,7 +103,7 @@ def display_personal_health(draw, image, x_start, y_start, health_data, name):
     image.paste(sleep_icon, (x_start, y), sleep_icon)
     
     sleep_hours = str(int(health_data['sleep_hours']))
-    sleep_minutes = str(round(health_data['sleep_hours'] % 1 * 60, 0))
+    sleep_minutes = str(int(health_data['sleep_hours'] % 1 * 60))
     sleep_score = str(health_data['sleep_score'])
     
     draw.text((x_start + icon_size + 5, y), sleep_hours + ':' + sleep_minutes + 'h -> ' + sleep_score + 'P' , font = font_small, fill = fill_main )
