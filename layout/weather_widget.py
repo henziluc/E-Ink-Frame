250437a729a555b36c99d28307bce79ed1a31cc6
lhenz <henzi.luca@gmail.com>
2026-09-23 21:28:02 +0200

    fixed error

--- layout/weather_widget.py
+++ layout/weather_widget.py
@@ -3,7 +3,7 @@ import math
 from PIL import Image
 from pathlib import Path
 
-from .fonts import font_very_small, font_small, font_medium, font_large, fill_main, fill_rain_graph, spacing_small, spacing_normal, spacing_large
+from .fonts import font_very_small, font_small, font_medium, font_large, fill_main, fill_gray, spacing_small, spacing_normal, spacing_large
 from .helpers import draw_centered_text, draw_smooth_curve, draw_dotted_line
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -183,7 +183,7 @@ def draw_rain_graph(draw, df_from_now, x_start, y_start, graph_height, hour_spac
         x2 = x_start + hour_spacing * i + math.floor(hour_spacing / 2)
         y1 = y_start + graph_height - precipitation * rain_spacing
         y2 = y_start + graph_height
-        draw.rectangle([(x1, y1),(x2, y2)], fill= fill_rain_graph)
+        draw.rectangle([(x1, y1),(x2, y2)], fill= fill_gray)
 
     
     # draw rain scale
