75956efe778eaff212cb968ce11015fc664040ad
lhenz <henzi.luca@gmail.com>
2026-09-25 21:42:34 +0200

    Fixed quote fetching

--- data/quote.py
+++ data/quote.py
@@ -5,9 +5,10 @@ from datetime import datetime
 url = "https://fixquotes.com/feeds/qotd.rss"
 
 def get_quote(qoutes):
-    if qoutes:
+    if qoutes is not None:
         return qoutes
     
+    qoutes = []
     response = requests.get(url)
     response.raise_for_status()
     
