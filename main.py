ffcd945dcbb13421a8edb231094744c8a052d9af
lhenz <henzi.luca@gmail.com>
2026-09-25 13:02:51 +0200

    added print of data after fetching

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def main():
             )
 
             logger.info("Data fetched successfully")
-            
+            print(data)
             # 2. Create and display dashboard
             data = make_dashbord(data)
 
